b4229dc025f2263d2771f1d40fdb444a71e6bd15
22A91A04O6 <119720290+22A91A04O6@users.noreply.github.com>
2022-12-11 18:00:22 +0530

    #include <stdio.h> int main() {     int cp,sp, amt;     scanf("%d", &cp);     scanf("%d", &sp);     if(sp > cp)     {         amt = sp - cp;         printf("Profit");     }     else if(cp > sp)     {         amt = cp - sp;         printf("Loss");     }     else     {         printf("No Profit No Loss.");     }
        return 0;
    }

---
+++ Profit.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int cp,sp, amt;
+    scanf("%d", &cp);
+    scanf("%d", &sp);
+    if(sp > cp)
+    {
+        amt = sp - cp;
+        printf("Profit");
+    }
+    else if(cp > sp)
+    {
+        amt = cp - sp;
+        printf("Loss");
+    }
+    else
+    {
+        printf("No Profit No Loss.");
+    }
+
+    return 0;
+}
